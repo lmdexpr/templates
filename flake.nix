bd133357d9fefb8423792cd87ab0510165dfc54e
lmdexpr <lmdexpr@gmail.com>
2025-04-29 20:47:36 +0900

    fix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
   };
-  outputs =
-    inputs@{ self, flake-parts, }:
+
+  outputs = inputs@{ self, systems, nixpkgs, flake-parts }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
       flake.templates = import ./template.nix;
     };
 }
